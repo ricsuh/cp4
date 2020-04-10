8d2606d25764f86d1cd1f31b9f88cbc469580591
ercsuh <ekjsuh@gmail.com>
2020-04-09 18:53:13 -0600

    Changed port number

--- back-end/server.js
+++ back-end/server.js
@@ -143,4 +143,4 @@ app.delete("/api/data/:id", async (req, res) => {
 })
 
 // Start app
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(4000, () => console.log('Server listening on port 4000!'));
